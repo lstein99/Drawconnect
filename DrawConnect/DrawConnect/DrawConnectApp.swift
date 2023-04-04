ec503e889e31a3d09594020c3868621b464bfc8f
lstein99 <129516217+lstein99@users.noreply.github.com>
2023-04-04 18:20:12 +0200

    Add files via upload

---
+++ DrawConnect/DrawConnect/DrawConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawConnectApp.swift
+//  DrawConnect
+//
+//  Created by Luc Steinert on 04.04.23.
+//
+
+import SwiftUI
+
+@main
+struct DrawConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawConnect/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  DrawConnect
+//
+//  Created by Luc Steinert on 04.04.23.
+//
+
+
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    // Data model
+    struct User {
+        let id: Int
+        let name: String
+    }
+    
+    struct Drawing {
+        let id: Int
+        let imageData: Data
+        let sender: User
+        let recipient: User
+    }
+    
+    // UI elements
+    let imageView = UIImageView()
+    let colorPicker = UISegmentedControl(items: ["Red", "Green", "Blue"])
+    let sendButton = UIBarButtonItem(title: "Send", style: .plain, target: ViewController.self, action: #selector(sendDrawing))
+    
+    // Properties
+    var currentUser: User?
+    var currentDrawing: Drawing?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up the UI layout
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        view.addSubview(colorPicker)
+        colorPicker.translatesAutoresizingMaskIntoConstraints = false
+        colorPicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        colorPicker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16).isActive = true
+        
+        navigationItem.rightBarButtonItem = sendButton
+        
+        // Set up gesture recognizer
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        imageView.addGestureRecognizer(panRecognizer)
+    }
+    
+    // Gesture recognizer handler
+    @objc func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        // Handle drawing on the image view
+        // ...
+    }
+    
+    // Button action
+    @objc func sendDrawing() {
+        // Use Apple's Push Notification service to send the drawing to the other user
+        // ...
+    }
+}
